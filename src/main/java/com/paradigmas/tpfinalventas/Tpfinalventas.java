6d0f5cd81e10334c6373d9302c9ac61c575f4cd6
Santiago Castro <santicastro0128@gmail.com>
2025-11-13 17:58:44 -0300

    mejora en la interfaz general

--- src/main/java/com/paradigmas/tpfinalventas/Tpfinalventas.java
+++ src/main/java/com/paradigmas/tpfinalventas/Tpfinalventas.java
@@ -1,10 +1,18 @@
 package com.paradigmas.tpfinalventas;
 
+import com.formdev.flatlaf.FlatDarculaLaf;
 import com.paradigmas.tpfinalventas.ui.Principal;
+
+import javax.swing.*;
 
 public class Tpfinalventas {
 
     public static void main(String[] args) {
+        try {
+            UIManager.setLookAndFeel( new FlatDarculaLaf() );
+        } catch( Exception ex ) {
+            System.err.println( "Failed to initialize LaF" );
+        }
         Principal inicio = new Principal();
         inicio.setVisible(true);
         inicio.setLocationRelativeTo(null);
